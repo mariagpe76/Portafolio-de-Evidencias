17e2565c22d8dcb3be18f7907347b1a8069f0c25
mariagpe76 <126023647+mariagpe76@users.noreply.github.com>
2025-04-08 13:06:58 -0600

    Add files via upload

---
+++ Sesion1/Ejemplo01/src/HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo{ //es una clase que se llama Hola Mundo
+    //Codigo de la clase
+    public static void main (String [] args){
+        System.out.println("¡Hola Mundo!");
+    }
+}
+//Main es la clase principal del codigo
+//Publico, privado y protejido - son los tipos de clases
+//El tipo de clase va a depender del encapsulamiento
---
+++ Sesion1/Retos_Sesión01/Reto02/src/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String[] args) {
+        Entrada entrada1 = new Entrada();
+        
+        entrada1.nombreEvento = "Obra de Teatro";
+        entrada1.precioEntrada = 450;
+
+        entrada1.mostrarInformacion();
+    }
+}

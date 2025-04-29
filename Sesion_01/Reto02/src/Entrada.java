ae8dfec759b47c95b7a7a0e65f1e50aa1fbb2228
mariagpe76 <126023647+mariagpe76@users.noreply.github.com>
2025-04-29 12:40:04 -0600

    Delete Sesion_01 directory
    Delete Sesion_01 directory

--- Sesion_01/Reto02/src/Entrada.java
+++
@@ -1,8 +0,0 @@
-public class Entrada {
-    String nombreEvento;
-    double precioEntrada;
-
-    public void mostrarInformacion() {
-        System.out.println("Evento: " + nombreEvento + " | Precio: $" + precioEntrada);
-    }
-}

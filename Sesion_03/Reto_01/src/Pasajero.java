dc9a0dc2b1ea004824b07d2340f198e72a5c6dd3
mariagpe76 <126023647+mariagpe76@users.noreply.github.com>
2025-05-02 20:17:18 -0600

    Retos Sesión 03
    Aquí se podrán encontrar las soluciones a los retos 01 y 02 de la sesión 03

---
+++ Sesion_03/Reto_01/src/Pasajero.java
@@ -0,0 +1,9 @@
+public class Pasajero {
+    String nombre;
+    String pasaporte;
+
+    public Pasajero(String nombre, String pasaporte) {
+        this.nombre = nombre;
+        this.pasaporte = pasaporte;
+    }
+}

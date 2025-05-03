c8edf08aae49491045f3b82555840b45589b59c4
mariagpe76 <126023647+mariagpe76@users.noreply.github.com>
2025-05-02 20:20:54 -0600

    Delete Sesion_03 directory
    Delete Sesion_03 directory

--- Sesion_03/Reto_01/src/Pasajero.java
+++
@@ -1,9 +0,0 @@
-public class Pasajero {
-    String nombre;
-    String pasaporte;
-
-    public Pasajero(String nombre, String pasaporte) {
-        this.nombre = nombre;
-        this.pasaporte = pasaporte;
-    }
-}
--- Sesion_03/Reto_02/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Factura f1 = new Factura(2500,"Servicio de consultoria","ABCC010101XYZ");
-        Factura f2 = new Factura(1800,"Reparación de equipo", null);
-
-        System.out.println(f1.getResumen());
-        System.out.println(f2.getResumen());
-    }
-}

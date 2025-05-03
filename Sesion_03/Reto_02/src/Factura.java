12f33f79900515fba21792bc440d776aced2934e
mariagpe76 <126023647+mariagpe76@users.noreply.github.com>
2025-05-02 20:21:53 -0600

    Retos Sesión 3
    Aquí se podrán encontrar las soluciones de los retos 01 y 02 de la sesión 3.

---
+++ Sesion_03/Reto_02/src/Factura.java
@@ -0,0 +1,21 @@
+import java.util.Optional;
+
+public class Factura {
+    private double monto;
+    private String descripcion;
+    private Optional<String> rfc;
+
+    public Factura(double monto, String descripcion, String rfc) {
+        this.monto = monto;
+        this.descripcion = descripcion;
+        this.rfc = Optional.ofNullable(rfc);
+    }
+    public String getResumen() {
+        String resumen = "📄 Factura generada:\n";
+        resumen += "Descripción: " + descripcion + "\n";
+        resumen += "Monto: " + monto + "\n";
+        resumen += "RFC: " + rfc.orElse("[No proporcionado]") + "\n";
+
+        return resumen;
+    }
+}
